4c8e82296396a3351a8e7259606a11e5878527cb
initialgproject <grupoinitialg@gmail.com>
2021-02-17 12:54:32 +0100

    añadir_ckeditor

--- views/noticias/_form.php
+++ views/noticias/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\datecontrol\DateControl;
+use dosamigos\ckeditor\CKEditor;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Noticias */
@@ -12,19 +14,24 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'autor_id')->textInput() ?>
+    <!-- $form->field($model, 'autor_id')->textInput()
+    $form->field($model, 'entradas_id')->textInput() -->
 
-    <?= $form->field($model, 'entradas_id')->textInput() ?>
+    <?= $form->field($model, 'titulo')->textarea(['rows' => 1]) ?>
 
-    <?= $form->field($model, 'titulo')->textarea(['rows' => 2]) ?>
+    <?= $form->field($model, 'descripcion')->textarea(['rows' => 3]) ?>
 
-    <?= $form->field($model, 'descripcion')->textarea(['rows' => 4]) ?>
+    <?= $form->field($model, 'texto')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
 
-    <?= $form->field($model, 'texto')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'imagen')->textarea(['rows' => 1]) ?>
 
-    <?= $form->field($model, 'imagen')->textarea(['rows' => 2]) ?>
-
-    <?= $form->field($model, 'fecha')->textInput() ?>
+    <?php /*$form->field($model, 'fecha')->widget(DateControl::classname(), [
+        'displayFormat' => 'php:d-M-Y H:i:s',
+        'type' => DateControl::FORMAT_DATETIME
+    ]); */?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
