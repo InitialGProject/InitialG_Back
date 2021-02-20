d7be6687a7ea1cfe9c162ce92195eecc11dba103
mithjack <alexlopez168@hotmail.com>
2021-02-20 19:26:56 +0100

    cambios form

--- views/juegos/_form.php
+++ views/juegos/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Categorias;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Juegos */
@@ -12,17 +14,23 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'titulo')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'titulo')->textarea(['rows' => 1]) ?>
 
-    <?= $form->field($model, 'descipcion')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'descipcion')->textarea(['rows' => 2]) ?>
 
-    <?= $form->field($model, 'imagen')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'imagen')->textarea(['rows' => 1]) ?>
 
-    <?= $form->field($model, 'categoria_id')->textInput() ?>
+    <?php $options = ArrayHelper::map(Categorias::find()->asArray()->all(), 'id', 'categoria'); ?>
+    <?=$form->field($model, 'categoria_id')->dropDownList($options, ['prompt' => 'Seleccionar']); ?>
 
-    <?= $form->field($model, 'tipo')->textInput(['maxlength' => true]) ?>
+    
+    <?=  $form->field($model, 'tipo')->dropdownList(
+            ['prompt' => 'Seleccionar'],
+            ['AC' => 'AC', 'RE' => 'RE']
+        ); 
+    ?>
 
-    <?= $form->field($model, 'ruta')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'ruta')->textarea(['rows' => 1]) ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
