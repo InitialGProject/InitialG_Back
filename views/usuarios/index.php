d5a06167d279f7b759850c9ece39d2adb30b26e0
mithjack <alexlopez168@hotmail.com>
2021-02-25 01:01:35 +0100

    quito pendientes

--- views/usuarios/index.php
+++ views/usuarios/index.php
@@ -24,7 +24,7 @@ $this->title = Yii::t('app', 'Usuarios');
             <?=
             Html::a(Yii::t('app', 'Crear Usuario'), ['create'], ['class' => 'btn btn-success']),
             Html::submitButton('Enviar', ['class' => 'btn btn-info',]),
-            Html::dropDownList('accion', '', ['' => 'Marcar selecc. como: ', 'A' => 'Aceptadas', 'D' => 'Denegado', 'P' => 'Pendientes'], ['class' => 'dropdown',]);
+            Html::dropDownList('accion', '', ['' => 'Marcar selecc. como: ', 'A' => 'Aceptadas', 'D' => 'Denegado'], ['class' => 'dropdown',]);
 
             ?>
 
